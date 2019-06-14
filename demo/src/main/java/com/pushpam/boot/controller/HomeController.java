60f870bab590a0b6d2811a9d173abcc7e575dc9f
Pushpam Ojha <pushpam.ojha@rsagroup.ca>
2019-06-14 16:41:56 +0530

    Initial Commit

---
+++ demo/src/main/java/com/pushpam/boot/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.pushpam.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("In Home");
+		return "hi";
+	}
+
+}
